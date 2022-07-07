62b73dcfbf93b9490e44cec43b6d9cd930a84a09
Gregor Adams <greg@pixelass.com>
2022-07-07 20:44:28 +0200

    refactor(@pforte/react): add types

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,7 +1,18 @@
 import { getSession } from "@pforte/client";
 import { useEffect, useState } from "react";
 
-export function useSession() {
+export interface PforteUser {
+	name: string;
+	email: null | string;
+	image: null | string;
+	id: string | number;
+}
+
+export interface PforteSession {
+	user: PforteUser;
+}
+
+export function useSession(): null | PforteSession {
 	const [session, setSession] = useState(null);
 
 	useEffect(() => {
