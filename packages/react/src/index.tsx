baae1f92e1ce6581c2d05ea1462f7f75b372ebd0
Gregor Adams <greg@pixelass.com>
2022-07-13 23:30:52 +0200

    docs: add doc-blocks

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -12,8 +12,16 @@ export interface PforteSession {
 	user: PforteUser;
 }
 
+/**
+ * Context to store the current session. The context is used to persist the
+ * session when unmounting and mounting components i.e. due to route changes
+ */
 export const SessionContext = createContext<null | PforteSession>(null);
 
+/**
+ * Looks for an active session and return it. Uses the context to persist the
+ * session.
+ */
 export function useSession(): null | PforteSession {
 	const [session, setSession] = useState(useContext(SessionContext));
 
@@ -26,6 +34,29 @@ export function useSession(): null | PforteSession {
 	return session;
 }
 
+/**
+ * Caches session in a context to persist it on route changes.
+ * This component should be wrapped around your router, we recommend using it as the outermost
+ * component in your app.
+ *
+ * @param children
+ *
+ * @example
+ * export default function App() {
+ * 	return (
+ * 		<SessionProvider>
+ * 			<BrowserRouter>
+ * 				<Routes>
+ * 					<Route path="/">
+ * 						<Route index element={<Home />} />
+ * 						<Route path="secret/" element={<Secret />} />
+ * 					</Route>
+ * 				</Routes>
+ * 			</BrowserRouter>
+ * 		</SessionProvider>
+ * 	);
+ * }
+ */
 export function SessionProvider({ children }: { children?: ReactNode }) {
 	const session = useSession();
 
